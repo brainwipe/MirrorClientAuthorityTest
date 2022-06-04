2dffc6d8591d1dc8d42b14f5e6baf73a6a30d927
Rob Lang <brainwiped@gmail.com>
2022-06-04 21:59:02 +0100

    Simplified the example

--- Assets/Scripts/TheCube.cs
+++ Assets/Scripts/TheCube.cs
@@ -17,18 +17,4 @@ public class TheCube : NetworkBehaviour
             transform.position = new Vector3(Mathf.Sin(Time.time) * 2, 0, 0);
         }
     }
-
-    [Server]
-    public void RemoveAuthority()
-    {
-        netIdentity.RemoveClientAuthority();
-        Debug.Log($"No player has authority, connection to client? {netIdentity.connectionToClient != null}");
-    }
-
-    [Server]
-    public void GiveAuthority(NetworkConnectionToClient playerConnection)
-    {
-        netIdentity.AssignClientAuthority(playerConnection);
-        Debug.Log($"Player {netIdentity.connectionToClient.connectionId} has authority");
-    }
 }
